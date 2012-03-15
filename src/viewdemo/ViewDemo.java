71b0251565b89aa121094739ce386b27471fe48a
Daniel Augurell <daniel.augurell@gmail.com>
2012-03-15 21:21:23 +0100

    Fixed screenresolution

--- src/viewdemo/ViewDemo.java
+++ src/viewdemo/ViewDemo.java
@@ -13,9 +13,9 @@ public class ViewDemo {
 	public static void main(String[] args) {
 		new ViewDemo();
 	}
-	
+
 	JFrame testFrame = null;
-	
+
 	public ViewDemo() {
 		System.out.println("Space is risky shiet");
 		System.out.println("Oh yea");
@@ -26,21 +26,23 @@ public class ViewDemo {
 		testFrame.setVisible(true);
 		testFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		testFrame.setIgnoreRepaint(true);
-		Timer timer = new Timer(1000/60, new MoveListener());
+		Timer timer = new Timer(1000 / 60, new MoveListener());
 		timer.start();
 	}
-	
+
 	private class MoveListener implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent event) {
 			Point loc = MouseInfo.getPointerInfo().getLocation();
-			if (loc.x == 1365) {
-				TestPanel.camera.x = Math.min(TestPanel.squareSize*24 - 1366, TestPanel.camera.x + 4);
+			if (loc.x == Toolkit.getDefaultToolkit().getScreenSize().getWidth() - 1) {
+				TestPanel.camera.x = Math.min(TestPanel.squareSize* 24 - (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth(), TestPanel.camera.x + 4);
 			} else if (loc.x == 0) {
 				TestPanel.camera.x = Math.max(0, TestPanel.camera.x - 4);
 			}
-			if (loc.y == 767) {
-				TestPanel.camera.y = Math.min(TestPanel.squareSize*23 - 748, TestPanel.camera.y + 4);
+			if (loc.y == Toolkit.getDefaultToolkit().getScreenSize()
+					.getHeight() - 1) {
+				TestPanel.camera.y = Math.min(TestPanel.squareSize * 23 - (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight(),
+						TestPanel.camera.y + 4);
 			} else if (loc.y == 0) {
 				TestPanel.camera.y = Math.max(0, TestPanel.camera.y - 4);
 			}
