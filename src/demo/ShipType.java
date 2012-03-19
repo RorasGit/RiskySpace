0cb965462441ca81f8bb74b0e7a7ace2f6865916
Alexander Hederstaf <alexander.hederstaf@gmail.com>
2012-03-19 19:40:11 +0100

    Implemented Territory and ShipType

--- src/demo/ShipType.java
+++ src/demo/ShipType.java
@@ -1,8 +1,47 @@
 package demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum ShipType {
-	SCOUT,
-	HUNTER,
-	DESTROYER,
-	COLONIZER;
+//			    Dmg Var HP  Atk  E
+	SCOUT		( 5, 3,  12, 1,  4),
+	HUNTER		(11, 3,  38, 1,  3),
+	DESTROYER	(11, 3, 100, 3,  3),
+	COLONIZER	( 1, 1,  38, 1,  2),
+	MAGIKARP	( 0, 0,   1, 0,  0); //FOR THE AWESOME
+	
+	private final int damage;
+	private final int variation;
+	private final int shield;
+	private final int energy;
+	private final int nbrAttacks;
+	
+	private ShipType(int damage, int variation, int shield, int nbrAttacks, int energy) {
+		this.damage = damage;
+		this.variation = variation;
+		this.shield = shield;
+		this.energy = energy;
+		this.nbrAttacks = nbrAttacks;
+	}
+	
+	/*
+	 * Return values may later be modified by a players Research.
+	 */
+	
+	public List<Integer> getDamage() {
+		List<Integer> damages = new ArrayList<Integer>();
+		for (int i = 0; i < nbrAttacks; i++) {
+			damages.add(damage + (int)(Math.random()*(variation+1)));
+		}
+		return damages;
+	}
+	
+	public int getShield() {
+		return shield;
+	}
+	
+	public int getEnergy() {
+		return energy;
+	}
 }
--- src/demo/Territory.java
+++ src/demo/Territory.java
@@ -1,8 +1,33 @@
 package demo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Territory {
 	private List<Fleet> fleets = null;
 	private Planet planet = null;
+	
+	public Territory() {
+		fleets = new ArrayList<Fleet>();
+	}
+	
+	public void addFleets(List<Fleet> fleets) {
+		for (int i = 0; i < fleets.size(); i++) {
+			this.fleets.add(fleets.get(i));
+		}
+	}
+	
+	/**
+	 * Set this territory to have a planet if there is not one already.
+	 * @param res The kind of Resource to be found the planet.
+	 * @return true if a planet was created
+	 */
+	public boolean setPlanet(Resource res) {
+		if (planet == null) {
+			planet = new Planet(res);
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
