4f45e34fdc9a880690ffa8564fb91532355f5040
Alexander Hederstaf <alexander.hederstaf@gmail.com>
2012-03-20 16:39:35 +0100

    New needed variables

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -2,9 +2,31 @@ package demo;
 
 public class Demo {
 	
+	/*
+	 * Players
+	 */
 	private Player[] players = {Player.BLUE, Player.RED};
 	private Player currentPlayer = null;
+//	private Map<Player, PlayerInfo> = null;
+	
+	/*
+	 * Models
+	 */
 	private World world = null;
+//	private Camera[] cameras = null;
+	
+	/*
+	 * Controllers
+	 */
+//	private GameThread gameThread = null;
+//	private CameraUpdater = null;
+//	TODO: Separated View and Model threads? 
+//	no threads and listeners instead somewhere?
+	
+	/*
+	 * Views
+	 */
+//	private View mainView = null;
 	
 	public Demo () {
 		world = new World();
