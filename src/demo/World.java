01c2b9958226e648ca2c5b26f061000ba9faf031
Daniel Augurell <daniel.augurell@gmail.com>
2012-03-20 14:06:43 +0100

    Added setPlanets in World

--- src/demo/World.java
+++ src/demo/World.java
@@ -1,26 +1,39 @@
 package demo;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class World {
 	private int rows = 0;
 	private int cols = 0;
 	private Map<Position, Territory> territories = null;
 	private Map<Player, PlayerStats> playerstats = null;
-	
+
 	public World(int rows, int cols) {
 		this.rows = rows;
 		this.cols = cols;
 		initTerritories();
 		initPlayers();
+		setPlanets();
 		
 		/*
-		 * Set Planets TODO: Roras
-		 * Set Starting Fleets
+		 * Set Planets TODO: Roras Set Starting Fleets
 		 */
 	}
-	
+
+	private void setPlanets() {
+		Object[] t = territories.values().toArray();
+		Random r = new Random();
+		int maxPlanets = 20;
+		for (int i = 0; i < maxPlanets; i++) {
+			((Territory) t[r.nextInt(400)]).setPlanet(i % 2 == 0 ? Resource.GAS
+					: Resource.METAL);
+		}
+
+	}
+
 	private void initTerritories() {
 		territories = new HashMap<Position, Territory>();
 		for (int row = 0; row < rows; row++) {
@@ -29,14 +42,18 @@ public class World {
 			}
 		}
 	}
-	
+
 	private void initPlayers() {
 		playerstats = new HashMap<Player, PlayerStats>();
 		playerstats.put(Player.BLUE, new PlayerStats());
 		playerstats.put(Player.RED, new PlayerStats());
 	}
-	
+
 	public World() {
-		this(20,20);
+		this(20, 20);
+	}
+
+	public static void main(String[] args) {
+		new World();
 	}
 }
