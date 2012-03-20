61407f207ac481a78511fa20977eab2c258e3286
Alexander Hederstaf <alexander.hederstaf@gmail.com>
2012-03-20 15:54:07 +0100

    takeDamage method added and documented

--- src/demo/Fleet.java
+++ src/demo/Fleet.java
@@ -3,6 +3,8 @@ package demo;
 import java.util.ArrayList;
 import java.util.List;
 
+import sun.org.mozilla.javascript.ast.FunctionNode.Form;
+
 public class Fleet {
 	
 	private Player owner = null;
@@ -26,11 +28,29 @@ public class Fleet {
 			}
 		}
 		return attacks;
-	}
+	} 
 	
 	/*
 	 * DEAL DAMAGE TO SHIPS METHOD TODO:
 	 */
+	/**
+	 * Attack this Fleet with attacks at target indexes.
+	 * @param attacks List of Integers of size n to represent each attack.
+	 * @param targetIndexes List of Integers of size n to represent each attacks target.
+	 * @throws IllegalArgumentException If the size of both lists are not the same.
+	 */
+	public void takeDamage(List<Integer> attacks, List<Integer> targetIndexes) {
+		if (attacks.size() != targetIndexes.size()) {
+			throw new IllegalArgumentException("Number of attacks must be equal to number of targets");
+		}
+		List<Ship> destroyedShips = new ArrayList<Ship>();
+		for (int i = 0; i < targetIndexes.size(); i++) {
+			if (ships.get(i).takeDamage(attacks.get(i))) {
+				destroyedShips.add(ships.get(i));
+			}
+		}
+		ships.removeAll(destroyedShips);
+	}
 	
 	public int shipCount(ShipType type) {
 		int nbrOfType = 0;
