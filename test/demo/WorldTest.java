7655c4ef26071ebda936307032a3092d84c7dab2
Daniel Augurell <daniel.augurell@gmail.com>
2012-03-22 14:37:14 +0100

    Test of Worlds planet generation.

---
+++ test/demo/WorldTest.java
@@ -0,0 +1,53 @@
+package demo;
+
+import static org.junit.Assert.*;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.util.Map;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
+
+import org.junit.Test;
+
+public class WorldTest {
+
+	@Test
+	public void testWorld() {
+		JFrame frame = new JFrame();
+		frame.setLayout(new GridLayout(20, 20));
+		frame.setSize(500, 500);
+		Map<Position,Territory> map = new World().getTerritories();
+		for (int row = 1; row <= 20; row++) {
+			for (int col = 1; col <= 20; col++) {
+				JPanel p = new JPanel();
+				p.setBorder(new LineBorder(Color.white));
+				if(map.get(new Position(row, col)).hasPlanet()){
+					p.setBackground(Color.red);
+				}else{
+					p.setBackground(Color.black);
+				}
+				frame.add(p);
+			}
+			System.out.println();
+		}
+
+		frame.setVisible(true);
+		while (frame.isVisible()) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+	}
+
+}
