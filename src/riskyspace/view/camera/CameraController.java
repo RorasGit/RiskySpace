cca93e436719af8bccf7b960d9e228fa2af40136
Daniel Augurell <daniel.augurell@gmail.com>
2012-03-29 09:06:26 +0200

    Broke out a line of code to a variable

--- src/riskyspace/view/camera/CameraController.java
+++ src/riskyspace/view/camera/CameraController.java
@@ -8,21 +8,24 @@ public class CameraController extends Thread {
 	
 	private static final long SLEEP_TIME = 1000/60;
 	private Camera currentCamera;
+	private double maximumXValue, maximumYValue;
 	
 	public void setCamera(Camera cam) {
 		currentCamera = cam;
+		maximumXValue = Toolkit.getDefaultToolkit().getScreenSize().getWidth()-1;
+		maximumYValue = Toolkit.getDefaultToolkit().getScreenSize().getHeight()-1;
 	}
 	
 	@Override
 	public void run() {
 		while(!isInterrupted()) {
 			Point loc = MouseInfo.getPointerInfo().getLocation();
-			if (loc.x >= Toolkit.getDefaultToolkit().getScreenSize().getWidth() - 1) {
+			if (loc.x >= maximumXValue) {
 				currentCamera.moveCamera(Camera.Direction.RIGHT);
 			} else if (loc.x <= 0) {
 				currentCamera.moveCamera(Camera.Direction.LEFT);
 			}
-			if (loc.y >= Toolkit.getDefaultToolkit().getScreenSize().getHeight() - 1) {
+			if (loc.y >= maximumYValue) {
 				currentCamera.moveCamera(Camera.Direction.DOWN);
 			} else if (loc.y <= 0) {
 				currentCamera.moveCamera(Camera.Direction.UP);
