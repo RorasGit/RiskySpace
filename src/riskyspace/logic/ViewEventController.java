135aa933845b073b8e967e74ac1b4f0aaefa8eda
Alexander Hederstaf <alexander.hederstaf@gmail.com>
2012-03-29 16:02:38 +0200

    Added support for selecting Fleets and setting Pahts for those selected

--- src/riskyspace/logic/ViewEventController.java
+++ src/riskyspace/logic/ViewEventController.java
@@ -1,7 +1,9 @@
 package riskyspace.logic;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import riskyspace.model.Colony;
 import riskyspace.model.Fleet;
@@ -17,8 +19,19 @@ import riskyspace.services.EventHandler;
 public class ViewEventController implements EventHandler {
 	
 	private World world = null;
-	private List<Fleet> selectedFleets = new ArrayList<Fleet>();
+	private Set<Fleet> selectedFleets = new HashSet<Fleet>();
+	private int fleetSelectionIndex = 0;
+	private Position lastFleetSelectPos = null;
 	private Colony selectedColony = null;
+	
+	private Map<Fleet, Path> fleetPaths = new HashMap<Fleet, Path>();
+	
+	/*TODO 
+	 * sätt Path med bara "startnod" (Spara startnod serparat som currentPos?)
+	 * lägg till targets stegvis (om shift)
+	 * spara alla selectedships med en Path i Map (ger även önskad hashSet effekt)
+	 * lägg bara till targets om det finns energi
+	 */
 	
 	public ViewEventController(World world) {
 		this.world = world;
@@ -27,31 +40,55 @@ public class ViewEventController implements EventHandler {
 
 	@Override
 	public void performEvent(Event evt) {
-		if (evt.getTag() == Event.EventTag.FLEET_SELECTED) {
-			resetVariables();
+		if (evt.getTag() == Event.EventTag.NEW_FLEET_SELECTION) {
 			/*
-			 * Is it List<Fleet> or Fleet
+			 * Only select one fleet
 			 */
-			if(evt.getObjectValue().getClass() == Fleet.class) {
-				selectedFleets.add((Fleet) evt.getObjectValue());
-			} else {
-				List<Fleet> list = (List<Fleet>) evt.getObjectValue();
-				for(int i=0; i<list.size(); i++) {
-					selectedFleets.add(list.get(i));
+			resetVariables();
+			
+			if(evt.getObjectValue() instanceof Position) {
+				Position pos = (Position) evt.getObjectValue();
+				if (lastFleetSelectPos == null || !lastFleetSelectPos.equals(pos)) {
+					lastFleetSelectPos = pos;
+					fleetSelectionIndex = 0;
+				}
+				if (world.getTerritory(pos).hasFleet()) {
+					Fleet fleet = world.getTerritory(pos).getFleet(fleetSelectionIndex); // Change this value somehow
+					selectedFleets.add(fleet);
+					fleetPaths.put(fleet, new Path(pos));
+					fleetSelectionIndex = (fleetSelectionIndex + 1) % world.getTerritory(pos).getFleets().size();
 				}
 			}
 			//TODO: Send event Draw circle around the fleet that is selected.
-			
 		}
 		
-		if (evt.getTag() == Event.EventTag.DESELECT) {
-			resetVariables();
+		if (evt.getTag() == Event.EventTag.ADD_FLEET_SELECTION) {
+			selectedColony = null;
+			Event event = new Event(Event.EventTag.HIDE_MENU, null);
+			EventBus.INSTANCE.publish(event);
+			if(evt.getObjectValue() instanceof Position) {
+				Position pos = (Position) evt.getObjectValue();
+				if (lastFleetSelectPos == null || !lastFleetSelectPos.equals(pos)) {
+					lastFleetSelectPos = pos;
+					fleetSelectionIndex = 0;
+				}
+				if (world.getTerritory(pos).hasFleet()) {
+					Fleet fleet = world.getTerritory(pos).getFleet(fleetSelectionIndex); // Change this value somehow
+					selectedFleets.add(fleet);
+					fleetPaths.put(fleet, new Path(pos));
+					fleetSelectionIndex = (fleetSelectionIndex + 1) % world.getTerritory(pos).getFleets().size();
+				}
+			}
+		}
+
+		if (evt.getTag() == Event.EventTag.SET_PATH) {
+			Position target = (Position) evt.getObjectValue();
+			for(Fleet fleet : selectedFleets) {
+				fleetPaths.get(fleet).setTarget(target);
+				System.out.println(fleetPaths.get(fleet));
+			}
 		}
 		
-		/*
-		 * selects the fleet if the territory has one, otherwise the colony.
-		 * if it has neither nothing is selected.
-		 */
 		if(evt.getTag() == Event.EventTag.COLONY_SELECTED) {
 			resetVariables();
 			Territory selectedTerritory = world.getTerritory((Position) evt.getObjectValue());
@@ -83,7 +120,10 @@ public class ViewEventController implements EventHandler {
 				}
 			}
 		}
-		
+
+		if (evt.getTag() == Event.EventTag.DESELECT) {
+			resetVariables();
+		}
 	}
 	
 	/*
