f9db19bdf6ede886bd0d41614a701574d974884c
Alexander Hederstaf <alexander.hederstaf@gmail.com>
2012-04-17 10:44:49 +0200

    Reduced volume of Sound. Does not work on linux

--- src/riskyspace/sound/Sound.java
+++ src/riskyspace/sound/Sound.java
@@ -7,6 +7,7 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.DataLine;
+import javax.sound.sampled.FloatControl;
 
 public class Sound {
 	public static synchronized void playSound(final String name) {
@@ -17,10 +18,15 @@ public class Sound {
 			DataLine.Info info = new DataLine.Info(Clip.class, format);
 			Clip clip = (Clip)AudioSystem.getLine(info);
 			clip.open(inputStream);
+			try {
+				FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+				gainControl.setValue(-20.0f);
+			} catch (IllegalArgumentException e) {
+				System.err.println("Volume control not supported!");
+			}
 			clip.start();
 		} catch (Exception e) {
 			e.printStackTrace();
-//			System.err.println(e.getMessage());
 			System.exit(1);
 		}
 	}
