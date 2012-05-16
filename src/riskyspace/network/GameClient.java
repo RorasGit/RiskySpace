4b1f3b65f1ffc658d520e49e02c84f39d2a9b93c
Alexander Hederstaf <alexander.hederstaf@gmail.com>
2012-05-16 20:27:47 +0200

    -Animation bugs fixed

--- src/riskyspace/network/GameClient.java
+++ src/riskyspace/network/GameClient.java
@@ -38,7 +38,7 @@ public class GameClient implements EventHandler {
 	private Socket socket = null;
 
 	public static void main(String[] args) {
-		new GameClient("129.16.178.72", 6013);
+		new GameClient("129.16.188.104", 6013);
 	}
 	
 	public GameClient(String hostIP, int hostPort) {
