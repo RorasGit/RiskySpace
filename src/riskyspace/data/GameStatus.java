6c2c1a289b4b6a3325a27715073ed73e43400438
Jonatan Rapp <rapp.jonatan@gmail.com>
2012-05-17 16:38:15 +0200

    move() fully implemented

--- src/riskyspace/data/GameStatus.java
+++ src/riskyspace/data/GameStatus.java
@@ -1,38 +1,48 @@
 package riskyspace.data;
 
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
+import riskyspace.model.Fleet;
 import riskyspace.model.Player;
 import riskyspace.model.Position;
+import riskyspace.model.Ship;
+import riskyspace.model.ShipType;
 import riskyspace.model.World;
 
-public class GameStatus implements Serializable {
+public class GameStatus {
 	
 	static File oldSave = new File("c:\\old_saved.txt");
 	static File newSave = new File("c:\\new_saved.txt");
 	
+	static Path pathOldSave = FileSystems.getDefault().getPath("c:\\old_saved.txt");
+	static Path pathNewSave = FileSystems.getDefault().getPath("c:\\new_saved.txt");
+	
 	static Position pos = new Position (1, 1);
-	
 
-	
-	public static void saveGame(World world, Player currentPlayer, int turn ) {
+	public static void saveGame(World world, Player currentPlayer, int turn ) throws ClassNotFoundException {
 		
 		try {
 			
+			move(pathNewSave, pathOldSave);
+			
 			FileOutputStream fos = new FileOutputStream(newSave);
 			ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+			for (int i = 0; i < 2000; i++) {
+				world.getTerritory(pos).addFleet(new Fleet(new Ship(ShipType.COLONIZER), Player.BLUE));
+			}
 			
-			World vorld = world;
-			
-			oos.writeObject(vorld);
-			
+			oos.writeObject(world);
 			oos.close();
 			
 		} catch (IOException ioe) {
@@ -41,20 +51,16 @@ public class GameStatus implements Serializable {
 		
 	}
 	
-	public static void move(File from, File to) {
-		
-		try {
-			
-			FileInputStream fis = new FileInputStream(from);
-			
-			
-			
-			
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		
+	public static void move(Path from, Path to) {
+				
+			try {
+				Files.move(from, to, REPLACE_EXISTING);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
 	}
+		
 	
 	public static void loadGame() throws IOException {
 		
@@ -64,14 +70,19 @@ public class GameStatus implements Serializable {
 			ObjectInputStream ois = new ObjectInputStream(fis);
 			
 			World world2 = (World) ois.readObject();
+			
+			ois.close();
+			
+			System.out.println("Content positions :" + world2.getContentPositions());
 		
 		} catch (ClassNotFoundException  e) {
 			System.out.println(e);
 		}
 	}
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		saveGame(new World(), Player.BLUE, 0);
+
 		loadGame();
 	}
 
